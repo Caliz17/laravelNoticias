68f6e2a6aa31a508ae29a8a159f6942ff55a89fe
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-04 20:28:50 -0600

    💄 Install daysiUi

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,12 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [daisyui],
+    daisyui: {
+        themes: false,
+        darkTheme: true,
+    },
+};
