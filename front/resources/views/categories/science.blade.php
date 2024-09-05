ecdfa09a52b77d5be7f7f12b320854bbff2db566
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-05 07:08:31 -0600

    ✏️ Add cards in categories all

--- front/resources/views/categories/science.blade.php
+++ front/resources/views/categories/science.blade.php
@@ -3,6 +3,27 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>entretenimiento</h1>
-@endsection
+    <div class="container mx-auto text-center mt-10">
+        <h1 class="text-5xl text-white font-bold">Ciencia</h1>
+        <div class="container mx-auto px-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        @foreach ($news as $new)
+            <x-card image="{{ $new['urlToImage'] }}" title="{{ $new['title'] }}" description="{{ $new['description'] }}"
+                loading="lazy">
+                @if ($new['source']['name'] != null)
+                    <span
+                        class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        {{ $new['source']['name'] }}
+                    </span>
+                @endif
+                <span
+                    class="inline-block bg-violet-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                    {{ Date::parse($new['publishedAt'])->diffForHumans() }}
+                </span>
+            </x-card>
+        @endforeach
+    </div>
+</div>
 
+    </div>
+@endsection
