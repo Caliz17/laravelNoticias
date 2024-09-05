9f58073ff0f75e059b4680c7ed0511dfe6af3362
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-04 18:40:02 -0600

    ✨ Create endpoint register user

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -4,8 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Container\Attributes\Log;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
-    //
+    public function userRegister(Request $request)
+    {
+        $status = null;
+        $message = null;
+        $success = null;
+        $request = $request->all();
+
+        // reglas
+        $validator = Validator::make($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator) {
+            $user = new User();
+            $user->name = $request['name'];
+            $user->email = $request['email'];
+            $user->password = Hash::make($request['password']);
+            $result = $user->save();
+            if ($result) {
+                $status = Response::HTTP_OK;
+                $message = 'Usuario registrado correctamente';
+                $success = true;
+            } else {
+                $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+                $message = 'Error al registrar usuario';
+                $success = false;
+                Log::error('Error al registrar usuario'. $validator->errors());
+            }
+        } else {
+            $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $message = 'Error al registrar usuario';
+            $success = false;
+            Log::error('Error al registrar usuario'. $validator->errors());
+        }
+
+        return response()->json([
+            'status' => $status,
+            'success' => $success,
+            'message' => $message,
+        ]);
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function (Request $request) {
     return response()->json(['message' => 'Welcome to api v 1.0!']);
 });
 
 Route::group(['middleware' => 'api'], function ($routes) {
-    Route::post('register-user', [Usercontroller::class,'userRegister']);
+    Route::post('register-user', [UserController::class,'userRegister']);
 });
