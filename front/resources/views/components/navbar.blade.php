14f1ec4dc1e3b3975060dcc354500d1b68ebb6cb
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-04 21:40:11 -0600

    💄 Change style navbar

--- front/resources/views/components/navbar.blade.php
+++ front/resources/views/components/navbar.blade.php
@@ -5,44 +5,49 @@
     <div class="navbar-start">
         <div class="dropdown">
             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-                </svg>
+                <i class="fas fa-bars text-xl text-white"></i>
             </div>
-            <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li><a>Item 1</a></li>
+            <ul tabindex="0"
+                class="menu menu-sm dropdown-content bg-sky-500 text-white rounded-box z-[1] mt-1 w-52  shadow">
                 <li>
-                    <a>Parent</a>
-                    <ul class="p-2">
+                    <a>Categorias</a>
+                    <ul class="rounded bg-sky-500">
                         <li><a>Submenu 1</a></li>
                         <li><a>Submenu 2</a></li>
                     </ul>
                 </li>
-                <li><a>Item 3</a></li>
             </ul>
         </div>
-        <!-- // agregar icono a la izquierda de noticias, con fontawesome -->
         <a class="btn btn-ghost text-white text-xl">
             <i class="fas fa-newspaper"></i>
             Noticias</a>
     </div>
-    <div class="navbar-center hidden lg:flex">
+    <div class="navbar-center hidden lg:flex text-white">
         <ul class="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
             <li>
                 <details>
-                    <summary>Parent</summary>
-                    <ul class="p-2">
+                    <summary>
+                        <i class="fas fa-folder"></i>
+                        Categorias
+                    </summary>
+                    <ul class="rounded bg-sky-500">
                         <li><a>Submenu 1</a></li>
                         <li><a>Submenu 2</a></li>
                     </ul>
                 </details>
             </li>
-            <li><a>Item 3</a></li>
         </ul>
     </div>
     <div class="navbar-end">
-        <a class="btn">Button</a>
+        <a class="btn mr-1">
+            <i class="fas fa-sign-in-alt"></i>
+            Acceder</a>
+
+        <a class="btn">
+            <i class="fas fa-user-plus"></i>
+            Registrarse</a>
+        <a class="btn">
+            <i class="fas fa-sign-out-alt"></i>
+            Cerrar Sesion</a>
     </div>
 </div>
