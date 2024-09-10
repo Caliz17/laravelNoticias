151fd2c6c0633f09629813a180544f2a319602a7
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-09 19:23:18 -0600

    💄 Change color ui

--- front/resources/views/categories/entertainment.blade.php
+++ front/resources/views/categories/entertainment.blade.php
@@ -13,35 +13,8 @@
                     </div>
                 @endif
                 @foreach ($news as $index => $new)
-                    <form action="{{ route('news.detail') }}" method="POST" class="news-form">
-                        @csrf
-                        <input type="hidden" name="newsData" value="{{ json_encode($new) }}">
-                        <input type="hidden" name="relatedNews" class="related-news-input" value="">
-
-                        <button type="submit"
-                            class="block max-w-sm rounded bg-sky-500 p-2 m-2 overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-                            <img class="w-full h-48 object-cover" loading="lazy" src="{{ $new['urlToImage'] }}"
-                                alt="Card image">
-                            <div class="px-4 py-4">
-                                <div class="font-bold text-xl mb-2">{{ $new['title'] }}</div>
-                                <p class="text-gray-700 text-base description">
-                                    {{ $new['description'] }}
-                                </p>
-                            </div>
-                            <div class="px-4 py-2">
-                                @if ($new['source']['name'] != null)
-                                    <span
-                                        class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                        {{ $new['source']['name'] }}
-                                    </span>
-                                @endif
-                                <span
-                                    class="inline-block bg-violet-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-                                    {{ Date::parse($new['publishedAt'])->diffForHumans() }}
-                                </span>
-                            </div>
-                        </button>
-                    </form>
+                    <x-card :route="route('news.detail')" :image="$new['urlToImage']" :title="$new['title']" :description="$new['description']" :source="$new['source']['name'] ?? null"
+                        :publishedAt="$new['publishedAt']" :newsData="$new" :relatedNews="$relatedNews ?? ''" />
                 @endforeach
             </div>
         </div>
--- front/resources/views/components/card.blade.php
+++ front/resources/views/components/card.blade.php
@@ -1,18 +1,32 @@
-<!-- resources/views/components/card.blade.php -->
-@props(['route', 'image', 'title', 'description', 'loading' => 'lazy'])
+@props(['route', 'image', 'title', 'description', 'source', 'publishedAt', 'newsData', 'relatedNews'])
 
-<a href="{{ $route }}" class="block w-full max-w-sm rounded bg-sky-500 p-2 m-2 overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105" style="width: 100% !important;">
-    <img class="w-full h-48 object-cover" loading="{{ $loading }}" src="{{ $image }}" alt="Card image" style="width: 100% !important;">
-    <div class="px-4 py-4">
-        <div class="font-bold text-xl mb-2">{{ $title }}</div>
-        <p class="text-gray-700 text-base description">
-            {{ $description }}
-        </p>
-    </div>
-    <div class="px-4 py-2">
-        {{ $slot }}
-    </div>
-</a>
+<form action="{{ $route }}" method="POST" class="news-form">
+    @csrf
+    <input type="hidden" name="newsData" value="{{ json_encode($newsData) }}">
+    <input type="hidden" name="relatedNews" class="related-news-input" value="{{ $relatedNews }}">
+
+    <button type="submit"
+        class="block max-w-sm rounded bg-gray-100 p-2 m-2 overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
+        <img class="w-full h-48 object-cover" loading="lazy" src="{{ $image }}" alt="Card image">
+        <div class="px-4 py-4">
+            <div class="font-bold text-xl mb-2">{{ $title }}</div>
+            <p class="text-gray-700 text-base description">
+                {{ $description }}
+            </p>
+        </div>
+        <div class="px-4 py-2">
+            @if ($source != null)
+                <span
+                    class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {{ $source }}
+                </span>
+            @endif
+            <span class="inline-block bg-violet-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                {{ \Carbon\Carbon::parse($publishedAt)->diffForHumans() }}
+            </span>
+        </div>
+    </button>
+</form>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
