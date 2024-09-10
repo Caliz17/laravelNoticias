0a0677e0834e189143f3e8937928b94d87e2de07
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-09 21:16:55 -0600

    ✅ Update endpoint

--- api/app/Http/Controllers/NewsController.php
+++ api/app/Http/Controllers/NewsController.php
@@ -9,17 +9,47 @@ class NewsController extends Controller
 {
     public function index()
     {
-        $api_key = env('NEWS_API_KEY');
-        $response = Http::get('https://newsapi.org/v2/top-headlines', [
-            'category' => 'general',
-                'country' => 'us',
-                'apiKey' => $api_key,
-                'pageSize' => 5,
-                'page' => 1,
-                'language' => 'en'
-        ]);
-        return response()->json($response->json());
+        $api_key = env('NEWSDATA_API_KEY');
+        $cacheKey = 'news_data';
+        $cacheTime = 30 * 60;
+
+        try {
+            $news = cache()->remember($cacheKey, $cacheTime, function () use ($api_key) {
+                $response = Http::get('https://newsdata.io/api/1/news', [
+                    'category' => 'top',
+                    'country' => 'us',
+                    'apikey' => $api_key,
+                    'language' => 'es',
+                ]);
+
+                if ($response->successful()) {
+                    $newsData = $response->json();
+                    $filteredNews = array_filter($newsData['results'], function ($item) {
+                        return !empty($item['image_url']) &&
+                               !empty($item['description']) &&
+                               !empty($item['creator']) &&
+                               is_array($item['creator']) &&
+                               !empty($item['creator'][0]);
+                    });
+                    return array_slice($filteredNews, 0, 5);
+                } else {
+                    throw new \Exception('Error en la solicitud de noticias: ' . $response->body());
+                }
+            });
+
+            return response()->json([
+                'news' => $news
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Se produjo un error al procesar la solicitud.',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
+
+
 
     public function showGeneral($page, $number)
     {
